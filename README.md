8b25ca722065feb073d0f26d936130a4186827dd
JAKE QUATTROCCHI <jake.quattrocchi@gmail.com>
2017-02-06 17:21:13 -0500

    added code

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # react-resize-to-aspect-ratio
 
 A react component that tracks if an element has changed size (a react wrapper for element-resize-event). Also it can resizes height/width to match aspect ratio
+
+
+Example Use:
+
+<Resize aspectRatio="16:9">
+  <div style={{ width: '100%' }}>
+    Hello World
+  </div>
+</Resize>
---
+++ index.js
@@ -0,0 +1,66 @@
+import React, { Component, PropTypes } from 'react';
+import elementResizeEvent from 'element-resize-event';
+
+export class Resize extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      element: null,
+    };
+
+    this.changeHeight = this.changeHeight.bind(this);
+    this.aspectRatioMultiplier = this.aspectRatioMultiplier.bind(this);
+    this.scaleToAspectRation = this.scaleToAspectRation.bind(this);
+  }
+
+  componentDidMount() {
+    const { onChange = () => {} } = this.props;
+    const element = document.querySelector('#resize');
+    this.scaleToAspectRation(element);
+    elementResizeEvent(element, (event) => {
+      onChange(element, event);
+      this.scaleToAspectRation(element);
+    });
+  }
+
+  aspectRatioMultiplier(ratio) {
+    if ((ratio.match(/:/g) || []).length !== 1) {
+      console.log('invalid aspect ratio');
+      return 0;
+    }
+    const ratioValues = ratio.split(':');
+    return ratioValues[1] / ratioValues[0];
+  }
+
+  scaleToAspectRation(element) {
+    const { aspectRatio } = this.props;
+    const arm = this.aspectRatioMultiplier(aspectRatio);
+    if (!arm) return;
+    this.changeHeight(element, element.offsetWidth * this.aspectRatioMultiplier(aspectRatio));
+  }
+
+  changeHeight(element, val) {
+    element.style.height = `${val}px`;
+    console.log(`${val}px`);
+  }
+
+  render() {
+    return (
+      <div id="resize" style={{ width: '100%' }}>
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+Resize.propTypes = {
+  children: PropTypes.node.isRequired,
+  aspectRatio: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+
+export default Resize;
+
